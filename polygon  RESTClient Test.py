6341029fdca813972190b618765e139fc32f2ac7
ntill22 <ntill@students.kennesaw.edu>
2024-04-21 20:49:30 -0400

    Manipulating Polygon data:  creating lists out of aggregate data. PysimpleGUI: Tested Demo GUI's for appropriate applications

--- polygon  RESTClient Test.py
+++ polygon  RESTClient Test.py
@@ -11,3 +11,20 @@ for a in client.list_aggs(ticker=ticker, multiplier=1, timespan="minute", from_=
 
 print(aggs)
 
+# Python3 code here creating class
+class bar:
+	def __init__(self, date, open, high, low, close, volume):
+		self.date = date
+		self.open = open
+		self.high = high
+		self.low = low
+		self.close = close
+		self.volume = volume
+
+# creating list
+list = []
+
+# appending instances to list
+list.append(bar('Akash', 2))
+
+
